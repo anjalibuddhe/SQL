6f14b19ee65b144654190fe1d704a250fe24e9b1
Anjali <102462571+anjalibuddhe@users.noreply.github.com>
2023-07-29 15:57:34 +0530

    Add files via upload

--- bookAward1.sql
+++ bookAward1.sql
@@ -23,12 +23,29 @@ CREATE TABLE Books (
     published_date VARCHAR(20),
     FOREIGN KEY (author_id) REFERENCES Authors(author_id)
 )
+
+
 alter table Book add published_date varchar(20)
 
 
 insert into Books values (1, 'A River Sutra ', 101, 200.99, '2020-01-29'),(2,'A Sense of Time',102,300.00,'9091-02-30')
 insert into Books values (3,'The India Story',103,399.00,'9090-08-09'),(4,'Queen of Fire',104,459.99,'2023-04-08'),
 (5,'Lal Salam',105,678.00,'2022-03-23'),(6,'Hear Yourself',106,349.99,'2018-05-09')
+
+CREATE TABLE AwardsMaster (
+    award_type_id INT PRIMARY KEY,
+    award_name VARCHAR(255) NOT NULL,
+    award_price DECIMAL(10, 2)
+)
+
+
+ insert into AwardsMaster values(201, 'Bellwether Prize', 1000.00)
+ insert into AwardsMaster values(202, 'National Book Awards', 29999.00)
+ insert into AwardsMaster values(203, 'Costa Book Awards', 19999.00)
+ insert into AwardsMaster values(204, 'Women Prize for Fiction', 25999.00)
+ insert into AwardsMaster values(205, 'Best Fiction Award', 39999.00)
+ insert into AwardsMaster values(206, 'BookBrowse Awards', 45999.00)
+
 
 CREATE TABLE Awards (
     award_id INT PRIMARY KEY,
@@ -50,18 +67,7 @@ insert into Awards values (5, 205, 105, 5, 2016)
 insert into Awards values (6, 206, 106, 6, 2010)
 select * from Awards
 
-CREATE TABLE AwardsMaster (
-    award_type_id INT PRIMARY KEY,
-    award_name VARCHAR(255) NOT NULL,
-    award_price DECIMAL(10, 2)
-)
 
- insert into AwardsMaster values(201, 'Bellwether Prize', 1000.00)
- insert into AwardsMaster values(202, 'National Book Awards', 29999.00)
- insert into AwardsMaster values(203, 'Costa Book Awards', 19999.00)
- insert into AwardsMaster values(204, 'Women Prize for Fiction', 25999.00)
- insert into AwardsMaster values(205, 'Best Fiction Award', 39999.00)
- insert into AwardsMaster values(206, 'BookBrowse Awards', 45999.00)
 
 
  
@@ -77,39 +83,130 @@ JOIN AwardsMaster am ON aw.award_type_id = am.award_type_id;
  
 --Write a query to update author name of authors whose book price is > 399.00. Prefix author name with ‘Gita’ .
 
-update Authors set author_name=CONCAT('Gita',author_name)
-where author_id in()
+update Authors
+set author_name = concat('Gita ', author_name)
+where author_id IN (
+    select author_id
+    from Books
+    where price > 399.00
+)
+
 
 
 
 --Write a query to display authors and number of books written by that author. Show author who has written maximum books at the top.
 
+select  a.author_name,count(b.book_id) as 'Book_count'
+from Authors a
+inner join Books b on  a.author_id=b.author_id
+group by a.author_name
+order by count(b.book_id) desc
+
+
+
 --Write a query to select book name with 5 th highest price.
-select book_name from Books order by price desc limit 1 offset 
+
+select * from Books order  by price desc offset 4 rows fetch next 1 rows only
+
+
 
 
 
 --Select list of books which have same price as book ‘Diary of Ann Frank’.
 
---Increase price of all books written by Mr. Chetan Bhagat by 5%.
+
+select book_id, book_name, author_id, price, published_date
+from Books
+where price = (
+    select price
+    from Books
+    where book_name = 'The India Story'
+)
+
+
+
+--Increase price of all books written by Mr. Chetan Bhagat by 5
+
+select author_name from Authors where author_name='Gita Mehta'
+
+UPDATE Books set price = price * 0.05 where author_id = 101
+
+
 
 --Show award names and number of books which got these awards. 
 
+
+select am.award_name, count(a.book_id) AS num_books 
+from AwardsMaster am
+left join Awards a on am.award_type_id = a.award_type_id
+group by am.award_name
+
+
+
 --Delete all books written by ‘Chetan Bhagat’ 
+select author_id
+from Authors
+where author_name = 'Prem Rawat'
+
+delete Books
+where author_id = 105
+
+
 
 --Create view to show name of book and year when it has received award. 
 
+create view AwardReceived 
+as
+select b.book_name  ,a.
+Books b
+inner join Awards a
+
+
+
 --Create trigger to ensure min price of any book must be Rs. 100. 
+
+
 
 --Increase price of book by 10% if that book has received any award.
 
+
+
+
+
 --Show names of author and number of books written by that Author.
+select au.author_name, count(b.book_id) as num_books_written
+from Authors au
+left join Books b on au.author_id = b.author_id
+group by au.author_id, au.author_name
+
+
 
 --Show names of authors whose books are published before year 2020.
 
+select distinct au.author_name
+from Authors au
+join Books b on au.author_id = b.author_id
+where b.published_date < '2020-01-01'
+
+
 --Show name of books which has published within 1 year after 15 August 2020.
 
+select book_name
+from Books
+where published_date between '2020-08-16' and '2021-08-15';
+
+--where published_date between '2018-05-09' and '2023-04-08'
+
+
 --Delete all authors whose no book is published in year 2020.
+
+delete from Authors
+where author_id NOT IN (
+    select distinct author_id
+    from Books
+    where published_date LIKE '%2020%'
+)
+
 
 
 
